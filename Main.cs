2d041ebe65e2fa70613222e7d1a009921e4b6adf
DeltaCommandos <116213383+DeltaCommandos@users.noreply.github.com>
2025-04-06 00:32:21 +0300

    Зум и элдер скроллс

--- Main.cs
+++ Main.cs
@@ -6,11 +6,22 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+
 public partial class Main : Node2D
 {
 	static MongoClient client = new MongoClient("mongodb://localhost:27017/");
 	static IMongoDatabase db = client.GetDatabase("FamilyGuy");
 	static IMongoCollection<PersonModel> people = db.GetCollection<PersonModel>("Person");
+	
+	private float zoomLevel = 1.0f;
+	private const float zoomSpeed = 0.1f;
+	private const float minZoom = 0.5f;
+	private const float maxZoom = 2.0f;
+	private Vector2 zoomCenter;
+
+	// Переменные для перемещения
+	private const float scrollSpeed = 20.0f;
+
 	public void InitPerson(PersonModel model)
 	{
 		PackedScene scene = (PackedScene)GD.Load("res://Scenes/Person.tscn");
@@ -20,23 +31,64 @@ public partial class Main : Node2D
 		Sprite2D sprite = person.GetChild<Sprite2D>(0);
 		sprite.Texture = (Texture2D)GD.Load(model.IsMale ? "res://Images/male.png" : "res://Images/female.png");
 	}
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 		PersonModel Cain = people.Find(p => p.Id == "67ef00c0330b5bbd5b57a19d").First();
 		Cain.SetSource(people);
-		Person Obyortka = new Person {model = Cain};
-		List<Person> ancestors = Obyortka.GetFamily(1,1);
+		Person Obyortka = new Person { model = Cain };
+		List<Person> ancestors = Obyortka.GetFamily(1, 1);
 		GD.Print(Cain.FirstName);
-		foreach(var person in ancestors)
+		foreach (var person in ancestors)
 		{
 			GD.Print(person.model.FirstName + " " + person.Generation);
 		}
 		InitPerson(Cain);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Обработка зума колесиком мыши только при зажатом Ctrl
+		float zoomFactor = 0.0f;
+		float scrollDirection = 0.0f;
+		
+		if (Input.IsActionJustReleased("zoom_in") && Input.IsKeyPressed(Key.Ctrl))
+		{
+			zoomFactor = zoomSpeed; 
+		}
+		else if (Input.IsActionJustReleased("zoom_out") && Input.IsKeyPressed(Key.Ctrl))
+		{
+			zoomFactor = -zoomSpeed; 
+		}
+		else if (Input.IsActionJustReleased("zoom_in"))
+		{
+			scrollDirection = scrollSpeed; // Перемещение вниз
+		}
+		else if (Input.IsActionJustReleased("zoom_out"))
+		{
+			scrollDirection = -scrollSpeed; // Перемещение вверх
+		}
+
+		if (zoomFactor != 0.0f)
+		{
+			// Получаем позицию мыши в мировых координатах до зума
+			zoomCenter = GetGlobalMousePosition();
+			
+			// Применяем зум
+			float oldZoom = zoomLevel;
+			zoomLevel = Mathf.Clamp(zoomLevel + zoomFactor, minZoom, maxZoom);
+			
+			// Вычисляем смещение для центрирования на курсоре
+			Vector2 offset = zoomCenter - Position;
+			Position += offset - (offset * zoomLevel / oldZoom);
+			
+			// Применяем масштаб
+			Scale = new Vector2(zoomLevel, zoomLevel);
+		}
+		else if (scrollDirection != 0.0f)
+		{
+			// Перемещаем сцену вверх или вниз
+			Position += new Vector2(0, scrollDirection);
+		}
 	}
 }
